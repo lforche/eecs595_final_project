a6a639854122d2828aa43883cf1ce7bf5f8a74d4
lforche <lindsey.forche@rockets.utoledo.edu>
2023-12-01 16:49:20 -0500

    curr code

--- finetune.py
+++ finetune.py
@@ -30,59 +30,61 @@ class SimpleDataCollator:
         labels = [feature['labels'] for feature in features]
         batch_size = len(features)
 
-        # Create a list of dictionaries, each containing the relevant tensor
         inputs = {
-            'input_ids': torch.cat([ex['input_ids'] for ex in features], dim=0),
-            'token_type_ids': torch.cat([ex['token_type_ids'] for ex in features], dim=0),
-            'attention_mask': torch.cat([ex['attention_mask'] for ex in features], dim=0),
+            'input_ids': torch.stack([ex['input_ids'] for ex in features]),
+            'attention_mask': torch.stack([ex['attention_mask'] for ex in features]),
             'labels': torch.tensor(labels, dtype=torch.int64).to(device),
         }
 
         return inputs
 
 def load_data(tokenizer, params):
-    label_mapping = {"entailment": 0, "contradiction": 1, "neutral": 2}
-
     def tokenize_function(data):
-        sentence1 = data['sentence1']
-        sentence2 = data['sentence2']
-        labels = label_mapping[data['label1']]
+        dialog_text_list = data['dialog_text_list']
+        h = data['h']
+        labels = data['entailment']
+        input_texts = [f"{dialog} {h}" for dialog in dialog_text_list]  # Adjust as needed
 
         tokenized_example = tokenizer(
-            sentence1,
-            sentence2,
+            input_texts,
             truncation=True,
             padding=True,
             return_tensors='pt',
         )
 
-        tokenized_example["labels"] = torch.tensor(labels, dtype=torch.int64).to(device)
-        return tokenized_example
+        return {
+            'input_ids': tokenized_example['input_ids'],
+            'attention_mask': tokenized_example['attention_mask'],
+            'labels': torch.tensor(labels, dtype=torch.int64).to(device),
+        }
+    dataset = load_dataset("sled-umich/Conversation-Entailment")
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
-    data_collator = SimpleDataCollator(
-        tokenizer,
-        max_length=params.max_length,
-        pad_to_multiple_of=params.pad_to_multiple_of
-    )
+    accepted_keys = ["input_ids", "attention_mask", "labels"]
 
-    train_data = pd.read_csv(params.train_dataset, delimiter='\t')  
-    validation_data = pd.read_csv(params.validation_dataset, delimiter='\t')  
-    test_data = pd.read_csv(params.test_dataset, delimiter='\t')  
+    for key in tokenized_datasets['validation'].features.keys():
+        if key not in accepted_keys:
+            tokenized_datasets['validation'] = tokenized_datasets['validation'].remove_columns(key)
 
-    train_tokenized_datasets = train_data.apply(tokenize_function, axis=1)
-    validation_tokenized_datasets = validation_data.apply(tokenize_function, axis=1)
-    test_tokenized_datasets = test_data.apply(tokenize_function, axis=1)
+    for key in tokenized_datasets['test'].features.keys():
+        if key not in accepted_keys:
+            tokenized_datasets['test'] = tokenized_datasets['test'].remove_columns(key)
+
+    tokenized_datasets.set_format("torch")
+    data_collator = SimpleDataCollator(tokenizer)
+
+    eval_dataset = tokenized_datasets["validation"]
+    eval_dataloader = DataLoader(eval_dataset, batch_size=params.batch_size, collate_fn=data_collator)
     
-    train_dataloader = DataLoader(train_tokenized_datasets, batch_size=params.batch_size, collate_fn=data_collator)
-    validation_dataloader = DataLoader(validation_tokenized_datasets, batch_size=params.batch_size, collate_fn=data_collator)
-    test_dataloader = DataLoader(test_tokenized_datasets, batch_size=params.batch_size, collate_fn=data_collator)
+    test_dataset = tokenized_datasets["test"]
+    test_dataloader = DataLoader(test_dataset, batch_size=params.batch_size, collate_fn=data_collator)
 
-    return train_dataloader, validation_dataloader, test_dataloader
+    return eval_dataloader, test_dataloader
 
-def finetune(model, train_dataloader, eval_dataloader, params):
-    num_training_steps = params.num_epochs * len(train_dataloader)
+def finetune(model, dataloader, params):
+    num_training_steps = params.num_epochs * len(dataloader)
     num_warmup_steps = 0
-    lr = 5e-5
+    lr = 9e-2
     optimizer = AdamW(model.parameters(), lr=lr)
     
     progress_bar = tqdm(range(num_training_steps))
@@ -97,7 +99,7 @@ def finetune(model, train_dataloader, eval_dataloader, params):
 
     for epoch in range(params.num_epochs):
         model.train()
-        for batch in train_dataloader:
+        for batch in dataloader:
             batch = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**batch)
             loss = outputs.loss
@@ -109,7 +111,7 @@ def finetune(model, train_dataloader, eval_dataloader, params):
             progress_bar.update(1)
     
         model.eval()
-        for batch in eval_dataloader:
+        for batch in dataloader:
             batch = {k: v.to(device) for k, v in batch.items()}
             with torch.no_grad():
                 outputs = model(**batch)
@@ -143,22 +145,19 @@ def test(model, test_dataloader, prediction_save='predictions.torch'):
 
 def main(params):
     tokenizer = AutoTokenizer.from_pretrained(params.model)
-    train_dataloader, validation_dataloader, test_dataloader = load_data(tokenizer, params)
+    validation_dataloader, test_dataloader = load_data(tokenizer, params)
 
-    model = AutoModelForSequenceClassification.from_pretrained(params.model, num_labels=3)
+    model = AutoModelForSequenceClassification.from_pretrained(params.model)
     model.to(device)
-    model = finetune(model, train_dataloader, validation_dataloader, params)
+
+    model = finetune(model, validation_dataloader, params)
 
     test(model, test_dataloader)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Finetune Language Model")
-
-    parser.add_argument("--train_dataset", type=str, default="train.tsv")
-    parser.add_argument("--validation_dataset", type=str, default="valid.tsv")
-    parser.add_argument("--test_dataset", type=str, default="test.tsv")
     parser.add_argument("--model", type=str, default="bert-base-cased")
-    parser. add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--batch_size", type=int, default=1)
     parser.add_argument("--num_epochs", type=int, default=3)
     parser.add_argument("--max_length", type=int, default=None)
     parser.add_argument("--pad_to_multiple_of", type=int, default=None)
